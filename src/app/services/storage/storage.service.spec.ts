6a3a9349efc37dd53c38084242340ae88077eba5
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-05-12 00:57:45 +0300

    Refactored and added tests for storage service

--- src/app/services/storage/storage.service.spec.ts
+++ src/app/services/storage/storage.service.spec.ts
@@ -2,11 +2,47 @@ import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
 
+let storageService: StorageService
+
 describe('StorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        StorageService
+      ]
+    });
+    storageService = TestBed.get(StorageService);
+  });
 
   it('should be created', () => {
     const service: StorageService = TestBed.get(StorageService);
     expect(service).toBeTruthy();
+  });
+
+  it('saveToLocalStorage() | getFromLocalStorage()', (doneFn) => {
+    const object = { test: 'test' }
+    storageService.saveToLocalStorage('test', object);
+    storageService.getFromLocalStorage('test').then((value) => {
+      expect(value).toEqual(object);
+      doneFn();
+    });
+  });
+
+  it('removeFromLocalStorage() | getFromLocalStorage()', (doneFn) => {
+    const object = { test: 'test' }
+    storageService.saveToLocalStorage('test', object);
+    storageService.removeFromLocalStorage('test').then((value) => {
+      expect(value).toEqual(undefined);
+      doneFn();
+    });
+  });
+
+  it('clearLocalStorage() | getFromLocalStorage()', (doneFn) => {
+    const object = { test: 'test' }
+    storageService.saveToLocalStorage('test', object);
+    storageService.clearLocalStorage().then((value) => {
+      expect(value).toEqual(undefined);
+      doneFn();
+    });
   });
 });
