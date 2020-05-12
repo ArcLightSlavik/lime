e4c715b43af75c357ad2002926ec78451d883529
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-05-13 00:20:26 +0300

    Added a datetime service

---
+++ src/app/services/datetime/datetime.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatetimeService {
+
+  constructor() { }
+
+  getCurrentDateTime(): Date {
+    return moment().toDate();
+  }
+}
