03df649c792bf9fb6129335aa80af0080ef4739d
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-05-14 01:03:34 +0300

    Correct save and display expenses into local storage

--- src/app/services/action/action.service.ts
+++ src/app/services/action/action.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
+
 import {DataService} from '../data/data.service';
+import {StorageService} from '../storage/storage.service';
 import {ExpenseInterface} from '../../interface/expenseInterface';
 
 @Injectable({
@@ -7,10 +9,17 @@ import {ExpenseInterface} from '../../interface/expenseInterface';
 })
 export class ActionService {
 
-    constructor(private dataService: DataService) {
+    constructor(private dataService: DataService, private storageService: StorageService) {
     }
 
     async createExpense(expense: ExpenseInterface): Promise<void> {
+        await this.storageService.saveExpenseToLocal(expense);
         return this.dataService.setExpenses(expense);
+    }
+
+    async getTodayExpenseFromLocal(): Promise<ExpenseInterface[]> {
+        return await this.storageService.getExpensesFromLocal().then((expenses: ExpenseInterface[]) => {
+            return expenses;
+        });
     }
 }
