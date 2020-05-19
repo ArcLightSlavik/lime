1a6c0b6ff369093f8760782891b3824bfb4fa782
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-05-19 23:47:04 +0300

    Use routerLink in login page

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -7,28 +6,17 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
     private loginForm: FormGroup = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required, Validators.min(8)])
     });
 
-    constructor(private router: Router) {
-    }
-
-    ngOnInit() {
+    constructor() {
     }
 
     submitLoginFrom(): void {
         console.log('login request');
-    }
-
-    navigateToRegister(): void {
-        this.router.navigateByUrl('auth/register');
-    }
-
-    navigateToForgotPassword(): void {
-        this.router.navigateByUrl('auth/forgot-password');
     }
 }
